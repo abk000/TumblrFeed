a3fad00e2700f9109ea4cbd95bb48f3dd69c8e38
Arslan <saeed.arslan07@gmail.com>
2018-10-01 20:33:13 -0400

    required functionality complete

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Arslan Saeed on 10/1/18.
+//  Copyright © 2018 Arslan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
---
+++ TumblrFeed/PhotosViewController.swift
@@ -0,0 +1,135 @@
+//
+//  PhotosViewController.swift
+//  TumblrFeed
+//
+//  Created by Arslan Saeed on 10/1/18.
+//  Copyright © 2018 Arslan Saeed. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var photosView: UITableView!
+    
+    var posts: [[String : Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photosView.delegate = self
+        photosView.dataSource = self
+        photosView.rowHeight = 350
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+     
+                
+        let responseDictionary = dataDictionary["response"] as! [String: Any]
+                
+        self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+        self.photosView.reloadData()
+                
+            }
+        }
+        
+        task.resume()
+        
+    }
+
+
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+ 
+        return posts.count
+    }
+    
+
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+      //  cell.textLabel?.text = "This is row \(indexPath.row)"
+        let post = posts[indexPath.row]
+        
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+        
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+
+            cell.photoView.af_setImage(withURL: url!)
+        }
+        
+        
+        
+        return cell
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
